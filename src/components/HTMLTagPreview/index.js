c0a5156bd945252c6f6daa8f04055fbfb16ae004
code-tieumomo <code.tieumomo@gmail.com>
2023-05-13 04:08:16 +0700

    Update post

--- src/components/HTMLTagPreview/index.js
+++ src/components/HTMLTagPreview/index.js
@@ -4,7 +4,6 @@ export function HTMLTagPreview() {
     return (
         <section>
             <iframe style={{ width: "100%", minHeight: "50rem" }} src="https://devdocs.io/html/element"></iframe>
-            <iframe style={{ width: "100%", minHeight: "50rem" }} src="https://htmlreference.io/"></iframe>
         </section>
     );
 }
